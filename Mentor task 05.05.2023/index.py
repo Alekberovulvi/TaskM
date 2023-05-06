4ba9e4338e26612f2890531045d00bba2da9291b
Alekberovulvi <ulvina@code.edu.az>
2023-05-06 17:25:53 +0400

    PythonFile

---
+++ Mentor task 05.05.2023/index.py
@@ -0,0 +1,15 @@
+# kenar_uzunlugu = float(input("kvadrat: "))
+# duzbucaqli = float(input("kenar: "))
+
+# alan = kenar_uzunlugu * duzbucaqli
+# cevre = 2 * (kenar_uzunlugu + duzbucaqli)
+
+# print("kvadrat alan:", alan)
+# print("kvadrat cevre:", cevre)
+
+maas = float(input("Maaş yaz: "))
+faiz = float(input("Faizide yaz: "))
+
+yenipul = maas + (maas * faiz / 100)
+
+print("emekhaqqi: ", yenipul)
